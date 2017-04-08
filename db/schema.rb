05c7127cfb4126dc6a96d281609c9f9aaf448ac8
cwy007 <chanweiyan007@gmail.com>
2017-04-08 14:24:35 +0800

    add more detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408004952) do
+ActiveRecord::Schema.define(version: 20170408062144) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
