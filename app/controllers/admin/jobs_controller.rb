8bf0f2c072825ebc0acd2cf4617d5a9afe8d646b
cwy007 <chanweiyan007@gmail.com>
2017-04-08 22:12:37 +0800

    actual button for hide

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :require_is_admin
   layout "admin"
-  
+
   def index
     @jobs = Job.all.order("created_at DESC")
   end
@@ -43,7 +43,19 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path, alert: "Job was deleted!"
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.is_hidden = false
+    @job.save
+    redirect_to :back
+  end
 
+  def hide
+    @job = Job.find(params[:id])
+    @job.is_hidden = true
+    @job.save
+    redirect_to :back
+  end
 
 
   private
