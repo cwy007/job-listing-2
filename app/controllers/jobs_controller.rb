1d8530e9e977c46fa8a9b9cce595827b360ce83a
cwy007 <chanweiyan007@gmail.com>
2017-04-11 16:42:58 +0800

    use case when to order upper_bound

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,14 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.where(is_hidden: false).order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.where(is_hidden: false).order('wage_upper_bound DESC')
+            else
+              Job.where(is_hidden: false).order("created_at DESC")
+            end
   end
 
   def show
@@ -11,7 +18,7 @@ class JobsController < ApplicationController
     if @job.is_hidden
       flash[:warning] = "This job already achieved"
       redirect_to root_path
-    end 
+    end
   end
 
   def new
